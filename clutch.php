398321a47f548079aced873e730ce199c172afc5
Bruno Mota <bruno12mota@gmail.com>
2025-05-09 17:48:14 +0100

    chore: restructure integrations with plugins into separate folder

--- clutch.php
+++ clutch.php
@@ -21,9 +21,7 @@ define('CLUTCHWP_URL', plugin_dir_url(__FILE__));
 
 define('CLUTCHWP_PRIORITY', 1000);
 
-require_once CLUTCHWP_DIR . '/includes/acf/module.php';
-require_once CLUTCHWP_DIR . '/includes/jetengine/module.php';
-require_once CLUTCHWP_DIR . '/includes/metabox/module.php';
+require_once CLUTCHWP_DIR . '/includes/integrations/module.php';
 require_once CLUTCHWP_DIR . '/includes/admin/module.php';
 require_once CLUTCHWP_DIR . '/includes/auth/module.php';
 require_once CLUTCHWP_DIR . '/includes/blocks/module.php';
@@ -34,4 +32,3 @@ require_once CLUTCHWP_DIR . '/includes/settings/module.php';
 require_once CLUTCHWP_DIR . '/includes/cache/module.php';
 require_once CLUTCHWP_DIR . '/includes/permalinks/module.php';
 require_once CLUTCHWP_DIR . '/includes/websites/module.php';
-require_once CLUTCHWP_DIR . '/includes/cf7/module.php';
---
+++ includes/integrations/module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file initializes the Clutch plugin and sets up the necessary integrations with other plugins
+ */
+namespace Clutch\WP\Integrations;
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+	exit();
+}
+
+require_once __DIR__ . '/plugins/acf/module.php';
+require_once __DIR__ . '/plugins/cf7/module.php';
+require_once __DIR__ . '/plugins/jetengine/module.php';
+require_once __DIR__ . '/plugins/metabox/module.php';
