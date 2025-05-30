bb66dc4063eff306b769b448ba6f034ae689c855
Luis Correia <hello@luisc.pt>
2025-05-30 16:38:11 +0100

    chore: port blocks changes to monorepo structure

---
+++ plugins/clutch-wp/includes/blocks/assets/clutch-gen/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'f2952c8bc99dd43c3f71');
--- plugins/clutch-wp/includes/rest/routes/blocks.php
+++ plugins/clutch-wp/includes/rest/routes/blocks.php
@@ -9,11 +9,9 @@ add_action('rest_api_init', function () {
 	register_rest_route('clutch/v1', '/block-styles', [
 		'methods' => 'GET',
 		'callback' => __NAMESPACE__ . '\\rest_get_block_styles',
-	]);
-
-	register_rest_route('clutch/v1', '/block-styles', [
-		'methods' => 'POST',
-		'callback' => __NAMESPACE__ . '\\rest_set_block_styles',
+		'permission_callback' => function () {
+			return current_user_can('edit_posts'); // Ensure the user has the appropriate capability
+		},
 	]);
 });
 
@@ -24,80 +22,25 @@ add_action('rest_api_init', function () {
  */
 function rest_get_block_styles()
 {
-	$block_styles = get_posts([
-		'post_type' => 'clutch_block_styles',
-		'posts_per_page' => -1,
-		'post_status' => 'publish',
-	]);
+	// Retrieve the selected host from user meta (user is already logged in due to
+	// permission callback)
+	$selected_host = get_user_meta(
+		get_current_user_id(),
+		'selected_clutch_host',
+		true
+	);
 
-	$response = [];
+	// Fetch block styles from remote json file
+	$json_url = esc_url($selected_host) . '/clutch/classes.json';
+	$json_content = @file_get_contents($json_url);
 
-	foreach ($block_styles as $block_style) {
-		$response[] = [
-			'id' => get_post_meta($block_style->ID, 'style_id', true),
-			'label' => $block_style->post_title,
-			'className' => get_post_meta(
-				$block_style->ID,
-				'style_classname',
-				true
-			),
-			'style' => $block_style->post_content,
-		];
+	// Check if the JSON content was retrieved successfully
+	if (!$json_content) {
+		return new \WP_Error('no_data', 'No data found', ['status' => 404]);
 	}
 
-	// @todo: ensure classes get sorted in the same order they're shown in Clutch
-	usort($response, function ($a, $b) {
-		return $a['id'] > $b['id'];
-	});
+	// Decode the JSON content
+	$block_styles = json_decode($json_content);
 
-	return new \WP_REST_Response($response);
-}
-
-/**
- * Updates or creates block styles based on the provided data.
- *
- * @param \WP_REST_Request $request The REST API request object.
- * @return \WP_REST_Response|\WP_Error A REST response indicating success or an error.
- */
-function rest_set_block_styles(\WP_REST_Request $request)
-{
-	$block_styles = $request->get_json_params();
-
-	if (!is_array($block_styles)) {
-		return new \WP_Error(
-			'invalid_block_styles',
-			'Block styles must be an array',
-			['status' => 400]
-		);
-	}
-
-	// @todo optimize by mutating the retrieved post instead of upserting
-	foreach ($block_styles as $block_style) {
-		// retrieve custom post for block style
-		$existing_block_style = get_posts([
-			'numberposts' => 1,
-			'fields' => 'ids',
-			'post_type' => 'clutch_block_styles',
-			'meta_key' => 'style_id',
-			'meta_value' => $block_style['id'],
-		]);
-
-		// create or update custom post for block style
-		if (isset($block_style['id'], $block_style['className'])) {
-			wp_insert_post([
-				'ID' => $existing_block_style ? $existing_block_style[0] : 0,
-				'post_type' => 'clutch_block_styles',
-				'post_title' =>
-					$block_style['label'] ?: $block_style['className'],
-				'post_content' => $block_style['style'],
-				'post_status' => 'publish',
-				'meta_input' => [
-					'style_id' => $block_style['id'],
-					'style_classname' => $block_style['className'],
-				],
-			]);
-		}
-	}
-
-	return new \WP_REST_Response(null, 200);
+	return new \WP_REST_Response($block_styles, 200);
 }
